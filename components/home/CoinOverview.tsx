e4fe84e1c7b89f867eb4f97ec9a9839faec8d386
hayahelen <hayahelen@gmail.com>
2026-03-16 14:36:19 +0200

    implement candlestick chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -2,34 +2,43 @@ import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
 import { CoinOverviewFallback } from "@/components/home/fallback";
+import CandlestickChart from "../CandlestickChart";
 
 export const CoinOverview = async () => {
-  let coin;
-
   try {
-    coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
+    const [coin, coinOHLCData] = await Promise.all([
+      await fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+
+      await fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        precision: "full",
+      }),
+    ]);
+    return (
+      <div id="coin-overview">
+        <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image
+              src={coin.image.large}
+              alt={coin.name}
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+            </div>
+          </div>
+        </CandlestickChart>
+      </div>
+    );
   } catch (error) {
     console.error("[CoinOverview] Failed to fetch coin overview", error);
     return <CoinOverviewFallback />;
   }
-
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1>
-            {formatCurrency(coin.market_data.current_price.usd, {
-              currency: "USD",
-            })}
-          </h1>
-        </div>
-      </div>
-    </div>
-  );
 };
